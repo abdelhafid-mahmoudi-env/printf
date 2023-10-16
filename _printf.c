54ca90d0aa52baa0b588d73d295036ebf80e1574
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-16 13:12:55 -0700

    foewhifioew

--- _printf.c
+++ _printf.c
@@ -2,6 +2,34 @@
 #include <unistd.h>
 
 /**
+* write_char - Writes a character to stdout.
+* @c: The character.
+* @count: The count of characters.
+*/
+void write_char(char c, int *count)
+{
+    write(1, &c, 1);
+    (*count)++;
+}
+
+/**
+* write_string - Writes a string to stdout.
+* @str: The string.
+* @count: The count of characters.
+*/
+void write_string(char *str, int *count)
+{
+    int len = 0;
+
+    while (str[len])
+    {
+   	 write(1, &str[len], 1);
+   	 len++;
+    }
+    (*count) += len;
+}
+
+/**
 * _printf - Our custom printf function.
 * @format: The format string.
 * @...: The values to format and print.
@@ -9,48 +37,31 @@
 */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
+    va_list args;
+    int i = 0, count = 0;
 
-	va_start(args, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
-		{
-			if (format[i + 1] == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				char *str = va_arg(args, char *);
-				int len = 0;
+    va_start(args, format);
 
-				while (str[len])
-				{
-					write(1, &str[len], 1);
-					len++;
-				}
-				count += len;
-			}
-			else if (format[i + 1] == '%')
-			{
-				write(1, &format[i + 1], 1);
-				count++;
-			}
-			i++;
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
-	}
-	va_end(args);
-	return (count);
+    while (format && format[i])
+    {
+   	 if (format[i] == '%' &&
+   		 (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
+   	 {
+   		 if (format[i + 1] == 'c')
+   			 write_char(va_arg(args, int), &count);
+   		 else if (format[i + 1] == 's')
+   			 write_string(va_arg(args, char *), &count);
+   		 else if (format[i + 1] == '%')
+   			 write_char('%', &count);
+   		 i++;
+   	 }
+   	 else
+   	 {
+   		 write_char(format[i], &count);
+   	 }
+   	 i++;
+    }
+    va_end(args);
+    return (count);
 }
-
 
--- main.h
+++ main.h
@@ -4,6 +4,8 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+void write_char(char c, int *count);
+void write_string(char *str, int *count);
 
 #endif /* MAIN_H */
 

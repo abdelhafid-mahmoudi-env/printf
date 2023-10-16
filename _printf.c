6abd4ed5b7434928e449d059675470f74e4a6204
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-16 13:40:48 -0700

    script added

--- _printf.c
+++ _printf.c
@@ -1,42 +1,49 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
 
 /**
-* _printf - Our custom printf function.
-* @format: The format string.
-* @...: The values to format and print.
-* Return: The number of characters printed.
+ * _printf - Our custom printf function.
+ * @format: The format string.
+ * @...: The values to format and print.
+ * Return: The number of characters printed.
 */
 int _printf(const char *format, ...)
 {
+	unsigned int i = 0, j = 0;
 	va_list args;
-	unsigned int i = 0, count = 0;
-	cf_t types[] = {
-		{'c', print_character},
-		{'s', print_string},
-		{'%', print_modulo},
-		{0, NULL}
-	};
-
-	if (format == NULL)
-		return (-1);
 
 	va_start(args, format);
-	while (format[i])
+
+	while (format && format[i])
 	{
 		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
 		{
-			count += types[format[i + 1] - 'c'].f(args);
-			i += 2;
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+			}
+			else if (format[i + 1] == 's')
+			{
+				char *str = va_arg(args, char *);
+				while (str && str[j])
+				{
+					_putchar(str[j]);
+					j++;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+			}
+			i++;
 		}
 		else
 		{
-			write(1, &format[i], 1);
-			count++;
-			i++;
+			_putchar(format[i]);
 		}
+		i++;
 	}
 	va_end(args);
-	return (count);
+	return (i);
 }
+
--- main.h
+++ main.h
@@ -3,22 +3,8 @@
 
 #include <stdarg.h>
 
-/**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-typedef struct convert
-{
-	char sym;
-	int (*f)(va_list);
-} cf_t;
-
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_character(va_list arg);
-int print_string(va_list arg);
-int print_modulo(va_list arg);
 
 #endif /* MAIN_H */
+

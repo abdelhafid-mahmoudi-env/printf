64097f3c0f2be541a957eb02a57b67c73e8d02b3
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-16 13:28:24 -0700

    script added

--- _printf.c
+++ _printf.c
@@ -1,67 +1,53 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
-* write_char - Writes a character to stdout.
-* @c: The character.
-* @count: The count of characters.
-*/
-void write_char(char c, int *count)
-{
-    write(1, &c, 1);
-    (*count)++;
-}
-
-/**
-* write_string - Writes a string to stdout.
-* @str: The string.
-* @count: The count of characters.
-*/
-void write_string(char *str, int *count)
-{
-    int len = 0;
-
-    while (str[len])
-    {
-   	 write(1, &str[len], 1);
-   	 len++;
-    }
-    (*count) += len;
-}
-
-/**
-* _printf - Our custom printf function.
-* @format: The format string.
-* @...: The values to format and print.
+* _printf - Produces output according to a format.
+* @format: A character string.
+* @...: Variable number of arguments.
+*
 * Return: The number of characters printed.
 */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
+	int index = 0;
+	int length = 0;
+	va_list arg;
 
-    va_start(args, format);
+	cf_t functions[] = {
+		{'c', print_character},
+		{'s', print_string},
+		{'%', print_modulo}
+	};
 
-    while (format && format[i])
-    {
-   	 if (format[i] == '%' &&
-   		 (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
-   	 {
-   		 if (format[i + 1] == 'c')
-   			 write_char(va_arg(args, int), &count);
-   		 else if (format[i + 1] == 's')
-   			 write_string(va_arg(args, char *), &count);
-   		 else if (format[i + 1] == '%')
-   			 write_char('%', &count);
-   		 i++;
-   	 }
-   	 else
-   	 {
-   		 write_char(format[i], &count);
-   	 }
-   	 i++;
-    }
-    va_end(args);
-    return (count);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(arg, format);
+
+	while (format[index])
+	{
+		if (format[index] == '%')
+		{
+			if (format[index + 1] == '\0')
+				return (-1);
+
+			for (int j = 0; j < 3; j++) /* Changed from 13 to 3 */
+			{
+				if (format[index + 1] == functions[j].chr)
+				{
+					length += functions[j].func(arg);
+					index += 2;
+					break;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[index]);
+			length++;
+			index++;
+		}
+	}
+	va_end(arg);
+	return (length);
 }
-
--- main.h
+++ main.h
@@ -3,9 +3,23 @@
 
 #include <stdarg.h>
 
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+typedef struct convert
+{
+	char sym;
+	int (*f)(va_list);
+} cf_t;
+
 int _printf(const char *format, ...);
-void write_char(char c, int *count);
-void write_string(char *str, int *count);
+int _putchar(char c);
+int print_character(va_list arg);
+int print_string(va_list arg);
+int print_modulo(va_list arg); /* Vous pouvez également passer void au lieu de va_list si vous n'utilisez pas arg */
 
 #endif /* MAIN_H */
 

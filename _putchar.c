356f1ddf0c4f2d7b6906c91adc37706a9e6f8bcf
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-17 00:12:12 -0700

    test

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
+#include "holberton.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
+ * 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -11,4 +12,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-

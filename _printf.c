912b2e92099a5ea4bad66c3039ab12f56abe7139
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-17 01:45:17 -0700

    fpwoejf

--- _printf.c
+++ _printf.c
@@ -5,12 +5,15 @@
  * @format: the format specifier
  * Return: the formated string
  */
+
 int _printf(const char *format, ...)
 {
 	int printed = 0;
 
 	va_list args;
+
 	va_start(args, format);
+
 	while (*format != '\0')
 	{
 		if (*format == '%')

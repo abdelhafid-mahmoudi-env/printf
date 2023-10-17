52441c5676bbd45488fd344d69caeb761410f9eb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-17 13:29:27 -0700

    greg

--- get_precision.c
+++ get_precision.c
@@ -10,14 +10,12 @@
 */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	int curr_i = *i + 1, precision = -1;
 
 	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
-
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -32,10 +30,10 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 		}
 		else
+		{
 			break;
+		}
 	}
-
 	*i = curr_i - 1;
-
 	return (precision);
 }

00537a7585d6b34b6ade8eb5527ee1be6262650c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-17 19:03:30 -0700

    test

--- get_precision.c
+++ get_precision.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-* get_precision - Calculates the precision for printing
-* @format: Formatted string in which to print the arguments
-* @i: List of arguments to be printed.
-* @list: list of arguments.
-*
-* Return: Precision.
-*/
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ *
+ * Return: Precision.
+ */
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+		return (-1);
 
 	precision = 0;
 
@@ -32,12 +32,12 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 		}
 		else
+		{
 			break;
+		}
 	}
 
 	*i = curr_i - 1;
 
 	return (precision);
 }
-
-

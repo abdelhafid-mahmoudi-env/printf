620dad8f64e54e21668e71ebef397c38620bef93
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-20 04:12:36 -0700

    prinnt 1

--- _puts.c
+++ _puts.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+* _puts - Prints a string to stdout.
+* @str: The string to be printed.
+* Return: None (void).
+*/
 int _puts(const char *str)
 {
-    int count = 0;
-    while (*str)
-    {
-        putchar(*str);
-        str++;
-        count++;
-    }
-    return count;
-}
+	int count = 0;
 
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+	return (count);
+}

440d7fe27b9a2f549895d36b4ed8aa447933a885
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-20 03:24:13 -0700

    alpha

--- _printf.c
+++ _printf.c
@@ -1,38 +1,33 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdarg.h>
 
-/**
- * _printf - Custom printf function
- * @format: The format string
- * @...: Variadic arguments
- *
- * Return: Number of characters printed (excluding null byte)
- */
-int _printf(const char *format, ...)
-{
-    va_list args;
-    int count = 0;
+int _printf(const char *format, ...) {
+  int count = 0;
+  va_list args;
 
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            count++;
-        }
-        else
-        {
-            format++;
-            if (*format == 'c')
-                count += print_char(args);
-            else if (*format == 's')
-                count += print_string(args);
-            else if (*format == '%')
-                count += print_percent(args);
-        }
-        format++;
+  va_start(args, format);
+  while (*format != '\0') {
+    if (*format == '%') {
+      format++;
+      switch (*format) {
+        case 'c':
+          count += putchar((char) va_arg(args, int));
+          break;
+        case 's':
+          count += puts(va_arg(args, char *));
+          break;
+        case '%':
+          count += putchar('%');
+          break;
+      }
+      format++;
+    } else {
+      count += putchar(*format);
+      format++;
     }
-    va_end(args);
-    return count;
+  }
+
+  va_end(args);
+  return count;
 }
+

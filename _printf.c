305bfbc17780782d068559db3e3890fc4af950f5
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-20 04:16:49 -0700

    prinnt 1

--- _printf.c
+++ _printf.c
@@ -9,42 +9,43 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    char c;
-    char *str;
-    va_start(args, format);
+	va_list args;
+	int count = 0;
+	char c;
+	char *str;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                str = va_arg(args, char *);
-                count += _puts(str);
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				count += _puts(str);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-    va_end(args);
-    return count;
+	va_end(args);
+	return (count);
 }
+

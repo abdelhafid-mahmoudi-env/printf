47bb3fe6d6c0a4fefa787c5ea42eb170edfd5489
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-20 03:46:17 -0700

    prinnt 1

--- _printf.c
+++ _printf.c
@@ -2,47 +2,50 @@
 #include <stdarg.h>
 
 /**
- * _printf - Custom printf function.
+ * _printf - Custom printf function that prints formatted output.
  * @format: The format string.
  * @...: Variable number of arguments.
- * Return: Number of characters printed.
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
+	int count = 0;
+	va_list args;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                count += _puts(str);
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return (count);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				count += _puts(str);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
